8234b9af9deb3e8de2a0181d0102cedb7f01f51d
Erleson <erlesonfreire@gmail.com>
2020-02-20 10:46:10 -0300

    Exercicio 2 - Aula3

---
+++ Aula3_Q2.py
@@ -0,0 +1,5 @@
+dias_da_semana = {"1":"Domingo", "2":"Segunda", "3":"Terça", "4":"Quarta","5":"Quinta","6":"Sexta", "7":"Sábado"}
+
+dia = input("Informe o número respectivo do dia: ")
+
+print(dias_da_semana[dia])
